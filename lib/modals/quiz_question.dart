2f33a18f901db90241e464a7f85ee27916c29813
vaishali mukati <vaishalimukativasuki@gmail.com>
2023-09-29 11:02:16 +0530

    added Questions

--- lib/modals/quiz_question.dart
+++ lib/modals/quiz_question.dart
@@ -3,4 +3,9 @@ class QuizQuestion {
 
   final String text;
   final List<String> answer;
+     List<String> getShuffledAnswer(){
+       final shuffleList = List.of(answer);
+             shuffleList.shuffle();
+       return shuffleList;
+     }
 }
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:adv_project/answer_button.dart';
 import 'package:adv_project/questions.dart';
+import 'package:adv_project/modals/quiz_question.dart';
 
 class QuestionScreen extends StatefulWidget{
    @override
@@ -10,27 +11,43 @@ class QuestionScreen extends StatefulWidget{
 }
 
 class _QuestionState extends State<QuestionScreen>{
+  var currentQIndex = 0;
+
+  void IncrementCurrentQ (){
+    setState(() {
+      currentQIndex++;
+    });
+  }
+
+
    @override
   Widget build( context) {
-     final currentQuestion = questions[0];
+     final currentQuestion = questions[currentQIndex];
 
-    return  SizedBox(
-            width: double.infinity,
-            child: Column(
-                   mainAxisAlignment:MainAxisAlignment.center,
-                   children:[
-                           Text(
-                                 currentQuestion.text,
-                                 style:const TextStyle(
-                                 color:Colors.white,
-                             ),
-                           ),
-                              const SizedBox(height: 30),
-                                ...currentQuestion.answer.map((answer){
-                                    return AnswerButton(answertext:answer,onTap:(){});
-                                    })
-                            ],
-      ),
+    return  SizedBox
+      (
+      width: double.infinity,
+        child:Container(
+           margin:const EdgeInsets.all(40),
+           child: Column(
+             mainAxisAlignment:MainAxisAlignment.center,
+             crossAxisAlignment:CrossAxisAlignment.stretch,
+             children:[
+               Text(
+                 currentQuestion.text,
+                 style:const TextStyle(
+                   color:Colors.white,
+                 ),
+                   textAlign:TextAlign.center,
+
+               ),
+               const SizedBox(height: 30),
+               ...currentQuestion.getShuffledAnswer().map((answer){
+                 return AnswerButton(answertext:answer,onTap:(){});
+               })
+             ],
+           ),
+        ),
     );
   }
 }
