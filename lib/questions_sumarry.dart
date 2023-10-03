c587b6fc82dc59c40aa4d00e5cf01f47daafdb4e
vaishali mukati <vaishalimukativasuki@gmail.com>
2023-10-03 14:19:33 +0530

    remove extra spaces

---
+++ lib/questions_sumarry.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class QuestionSummary extends StatelessWidget{
+  const QuestionSummary(this.sumarryData,{super.key});
+
+  final List<Map<String,Object>> sumarryData;
+
+  @override
+  Widget build( context) {
+     return Column(
+             children:sumarryData.map(
+                     (data) {
+                    return  Row(children: [
+                        Text(((data['questions_index'] as int)+ 1).toString()),
+                        Expanded(child:
+                           Column(children: [
+                            Text(data ['questions'] as String),
+                           const SizedBox(height: 5),
+                            Text(data['user_Answer'] as String),
+                            Text(data['correct_Answer'] as String),
+                        ],)
+                       )
+                    ],
+                    );
+             }).toList()
+
+     );
+  }
+}
--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,12 +1,28 @@
+import 'package:adv_project/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_project/questions_sumarry.dart';
 
 class ResultScreen extends StatelessWidget{
   const ResultScreen({super.key, required this.chosenAnswer});
 
   final List<String> chosenAnswer;
 
+ List<Map<String,Object>> getSummaryData(){
+   final List<Map<String,Object>> summary = [];
 
-  @override
+   for(int i = 0; i < chosenAnswer.length;i++){
+     summary.add(
+              {
+            'questions_index' : i,
+            'questions':questions[i].text,
+            'correct_Answer':questions[i].answer[0],
+            'user_Answer':chosenAnswer[i],
+             },
+     );
+   }
+  return summary;
+ }
+   @override
   Widget build(context) {
      return  SizedBox(
          width: double.infinity,
@@ -15,9 +31,9 @@ class ResultScreen extends StatelessWidget{
             child:Column(
               mainAxisAlignment:MainAxisAlignment.center,
              children: [
-              const Text('Your answer x out of Y Questions correctly'),
-              const SizedBox(height:30),
-              const Text('List of Answer and questions'),
+              QuestionSummary(getSummaryData()),
+              //const SizedBox(height:30),
+              //const Text('List of Answer and questions'),
                const SizedBox(height:30),
                TextButton(
                    onPressed:(){},
